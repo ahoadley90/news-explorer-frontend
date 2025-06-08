4a2d20b0cfa4a630afce5bb4b68c8dd4eee57b3e
AmandaKim <akimmets@gmail.com>
2025-06-07 21:01:43 -0500

    initial project structure and basic files

---
+++ src/utils/api.js
@@ -0,0 +1,8 @@
+// This file will contain your API calls
+const BASE_URL = "https://newsapi.org/v2";
+const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
+
+// Example function (you'll implement this later)
+export const getNews = (keyword) => {
+  // Implementation will go here
+};
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+
+// This file will contain your constant values
+
+export const MAX_CARDS = 100; // Example constant
